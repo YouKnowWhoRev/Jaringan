54c80f86675ff372bbe137528dfa97230c44e762
YouKnowWhoRev <114790930+YouKnowWhoRev@users.noreply.github.com>
2022-10-01 11:26:19 +0800

    Stabil-Jaringan

---
+++ stabil-jaringan.sh
@@ -0,0 +1,43 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay By ???
+echo $red"Welcome!!"
+echo
+sleep 2
+echo $blue"Silahkan Pilih Toolsnya :"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $green"0.) Exit"
+echo
+echo $white
+read -p "Pilih : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay ???
+echo $green"Stabilkan Jaringan By YouKnowWho"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "GoodBye"
+echo $white"Still In Progress.."
+echo $white"Thanks For Using!"
+echo $white"Contact Me 085341539009"
+sleep 3
+exit
+fi
